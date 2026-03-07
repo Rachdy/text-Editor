9d26e07cf10dca4e1d686e4d7bf073ad3628530a
Rachdy <ismailrchdy123@gmail.com>
2026-03-07 02:49:21 +0100

    add some changes

--- src/texte editor.cpp
+++ src/texte editor.cpp
@@ -3,22 +3,39 @@
 #include<string>
 #include<fstream>
 
-int main()
+int main(int argc, char* argv[])
 {
-    std::string file_name;
-    std::cout << "enter File name:";
-    std::cin >> file_name;
-    std::fstream file(file_name, std::ios::in);
-    if (file.is_open()) {
-        std::string s;
-        while (getline(file, s)) {
-            std::cout << s << "\n";
 
+    if (argc >= 2) {
+        bool continues = true;
+        while (true) {
+            std::fstream file(argv[1], std::ios::in);
+            if (file.is_open()) {
+                std::string s;
+                while (getline(file, s)) {
+                    std::cout << s << "\n";
+
+                }
+                file.close();
+            }
+            else {
+                std::cout << "Unable to open file.\n";
+            }
+
+            std::string user_input;
+            do {
+                std::cout << "Enter :quit to quit or :continue to read other file:";
+                std::cin >> user_input;
+            } while (user_input != ":quit" || user_input != ":continue");
+            if (user_input == ":quit") {
+                break;
+            }
+            else continue;
         }
-        file.close();
     }
+
     else {
-        std::cout << "Unable to open file.\n";
+        std::cout << "missing file name\n";
     }
 
     return 0;
